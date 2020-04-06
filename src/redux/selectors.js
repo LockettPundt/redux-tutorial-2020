20aeeb0608fc56c3c7285339f0d999eeb657bce3
Lockett Pundt <kalifornium@hotmail.com>
2020-04-06 14:20:10 -0400

    added a function to sort the todo list by the visibility. whether it was completed or not.

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,11 +1,28 @@
+import { VISIBILITY_FILTERS } from "../constants";
+
 export const getTodoList = store =>
   store && store.todos ? store.todos.allIds : [];
 
-export const getTodoById = (store, id) =>
+export const getTodoById = (store, id) => 
   store && store.todos && store.todos.byIds
     ? { ...store.todos.byIds[id], id }
     : {};
 
+
+export const getTodosByVisibilityFilter = (store, visibilityFilter) => {
+  const allTodos = getTodos(store);
+  switch (visibilityFilter) {
+    case VISIBILITY_FILTERS.COMPLETED:
+      return allTodos.filter(todo => todo.completed);
+    case VISIBILITY_FILTERS.INCOMPLETE:
+      return allTodos.filter(todo => !todo.completed);
+    case VISIBILITY_FILTERS.ALL:
+    default:
+      return allTodos;
+  }
+}
+
+    
 /**
  * example of a slightly more complex selector
  * select from store combining information from multiple reducers
